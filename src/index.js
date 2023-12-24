520e020079fdb3ee11894fc1f8100e2108d22e87
Abud <abdulrazakdaas0@gmail.com>
2023-12-24 18:06:12 +0300

    sphere

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
-
+// import bimg from '../img/download.jpg';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 
     75, window.innerWidth / window.innerHeight, 0.1, 1000 
 );
-camera.position.z = 5;
+camera.position.z = 4;
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -18,12 +18,27 @@ document.body.appendChild( renderer.domElement );
 // scene.add( cube );
 //===========================
 // Sphere
+const geometry = new THREE.SphereGeometry( 1, 99, 99 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const Sphere = new THREE.Mesh( geometry, material );
+scene.add( Sphere );
+
+
+// Background Img
+// const textureLoader = new THREE.TextureLoader();
+// const backgroundTexture = textureLoader.load("./src/back.jpg");
+// scene.background = backgroundTexture;
+renderer.setClearColor(0xffff00);
+
+
+
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+	// Sphere.rotation.x += 0.01;
+	// Sphere.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
